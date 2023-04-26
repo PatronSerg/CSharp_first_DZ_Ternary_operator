f1de2c8d5b3fc1ae65ef0b9a87b637c078730971
Serg <patron.serg@yandex.ru>
2023-04-26 21:29:36 +0300

    ввод

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,3 +3,9 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
