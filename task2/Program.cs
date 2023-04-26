ded17a3a6bfe924916d29fdc384660077e774da6
Serg <patron.serg@yandex.ru>
2023-04-26 21:48:20 +0300

    finishTask2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,7 +5,10 @@
 // a = -9 b = -3 -> max = -3
 
 Console.WriteLine("Введите первое число");
-int num = Convert.ToInt32(Console.ReadLine());
-
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a > b ? a : b; // Тернарный оператор  https://devpractice.ru/c-sharp-lesson-6-if-and-for/
+int min = a < b ? a : b; // Синтаксис оператора : "condition ? if_true : if_else"
+Console.WriteLine($"Наибольшее число: {max}");
+Console.WriteLine($"Наименьшее число: {min}");
