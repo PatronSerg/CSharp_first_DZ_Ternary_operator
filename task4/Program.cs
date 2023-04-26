ec73d7e4722f4ea46c9cc254a00cf6a90d26f14f
Serg <patron.serg@yandex.ru>
2023-04-26 22:19:17 +0300

    почти

--- task4/Program.cs
+++ task4/Program.cs
@@ -6,10 +6,10 @@
 
 Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите блять второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
-int с = Convert.ToInt32(Console.ReadLine());
-
-
+int c = Convert.ToInt32(Console.ReadLine());
+int d = a > b ? a : b; // Тернарный оператор  https://devpractice.ru/c-sharp-lesson-6-if-and-for/
+int max = c > d ? c : d;  // Синтаксис оператора : "condition ? if_true : if_else"
 Console.WriteLine($"Наибольшее число: {max}");
