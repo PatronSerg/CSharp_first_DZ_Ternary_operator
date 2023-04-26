991a0ea137336b5b4d2692ebe0ebe2fd3fc4e21a
Serg <patron.serg@yandex.ru>
2023-04-26 22:20:31 +0300

    finish4

--- task4/Program.cs
+++ task4/Program.cs
@@ -6,7 +6,7 @@
 
 Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите блять второе число");
+Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int c = Convert.ToInt32(Console.ReadLine());
