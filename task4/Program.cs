9cebe7dbba100a5c537d8776661a54e73bee70f0
Serg <patron.serg@yandex.ru>
2023-04-26 22:06:45 +0300

    это_база

--- task4/Program.cs
+++ task4/Program.cs
@@ -3,3 +3,13 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int с = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Наибольшее число: {max}");
